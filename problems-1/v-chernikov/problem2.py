8204b24c8cb0ccc76e8c02d32f2800b3cb7d9158
SpykeX3 <spykex3@gmail.com>
2021-04-05 22:46:48 +0700

    add exceptions to problem-1-2

--- problems-1/v-chernikov/problem2.py
+++ problems-1/v-chernikov/problem2.py
@@ -2,12 +2,12 @@
 
 def cut_off(min_value, max_value, args):
     if max_value < min_value:
-        raise RuntimeError
+        raise ValueError("Invalid input")
     return list(map(lambda x: max(min(max_value, x), min_value), args))
 
 
 if __name__ == '__main__':
     nums = list(map(int, input().split(' ')))
     if len(nums) < 2:
-        print("Not enough arguments")
+        raise ValueError("Not enough arguments")
     print(cut_off(nums[0], nums[1], nums[2:]))
