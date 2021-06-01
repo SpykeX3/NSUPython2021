e2d7464adc04dc6aa1039a1ea0d7dabb46d30ae4
SpykeX3 <spykex3@gmail.com>
2021-06-01 10:00:15 +0700

    fix error handling

--- problems-1/v-chernikov/problem2.py
+++ problems-1/v-chernikov/problem2.py
@@ -5,19 +5,23 @@ import sys
 
 def cut_off(min_value, max_value, args):
     if max_value < min_value:
-        raise ValueError("Invalid input, max_value is lower than min_value")
+        raise ValueError("Invalid input, max_value is lower than min_value: " + str(max_value) + "<" + str(min_value))
     return list(map(lambda x: max(min(max_value, x), min_value), args))
 
 
 def print_help():
-    print("Illegal input, provide a sequence of decimal integers separated with spaces:")
+    print("Provide a sequence of decimal integers separated with spaces:")
     print("max_value min_value [value1 [value2 [...]]]")
 
 
 if __name__ == '__main__':
     try:
-        nums = list(map(int, input().split(' ')))
+        try:
+            nums = list(map(int, input().split(' ')))
+        except ValueError as e:
+            raise ValueError("Error while parsing input numbers: " + str(e))
         if len(nums) < 2:
+            sys.stderr.write("Not enough arguments\n")
             print_help()
         else:
             try:
